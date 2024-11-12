eb1b5c6cc604a86ab8a23ea59b2f45a2d92e2548
iker <iker0592@gmail.com>
2024-11-12 23:05:10 +0100

    added calendar

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
-
-Amplify.configure(outputs);
+import React from 'react';
+import { Calendar } from '@/components/ui/calendar';
 
 export default function App() {
-
   return (
-      <div>
-        🥳 Thegent is here!
-      </div>
+    <div>
+      <Calendar />
+    </div>
   );
 }
